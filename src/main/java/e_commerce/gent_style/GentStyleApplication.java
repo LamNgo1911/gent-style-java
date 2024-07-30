952b5b39ed3424385b3e10f79d91f75244b3d6d1
Diddayngo <lamngo606@gmail.com>
2024-07-30 13:06:16 +0300

    added architecture and ERD

---
+++ src/main/java/e_commerce/gent_style/GentStyleApplication.java
@@ -0,0 +1,13 @@
+package e_commerce.gent_style;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GentStyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GentStyleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/e_commerce/gent_style/GentStyleApplicationTests.java
@@ -0,0 +1,13 @@
+package e_commerce.gent_style;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GentStyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
