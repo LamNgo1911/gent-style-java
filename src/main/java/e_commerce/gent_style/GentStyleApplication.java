c69a4498aea4ae87b590760f8221dc550a8c9a1c
Diddayngo <lamngo606@gmail.com>
2024-07-30 13:56:40 +0300

    checkinh git push

--- src/main/java/e_commerce/gent_style/GentStyleApplication.java
+++ src/main/java/e_commerce/gent_style/GentStyleApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GentStyleApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(GentStyleApplication.class, args);
+
+		System.out.println("hello");
 	}
 
 }
